696ea1d628a75c9843d4a0d3013fe8d1fe3b5f1f
prajapatirahul165 <121180105+prajapatirahul165@users.noreply.github.com>
2024-12-12 13:43:16 +0530

    Delete Netflix directory

--- Netflix/script.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('nav');
-
-document.addEventListener('scroll', (e) =>{
-    if(window.scrollY > 120){
-        nav.style.background = '#171717'
-    }else{
-        nav.style.background = ''
-    }
-})
